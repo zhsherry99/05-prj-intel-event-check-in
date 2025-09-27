ae6abd228711b3d078bb47df78be1ada64ef8763
Qianyu Zhang <zhsherry@umich.edu>
2025-09-27 21:56:22 +0000

    created workspace and made first changes

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('checkInForm');
+const nameInput = document.getElementById('attendeeName');
+const teamSelect = document.getElementById('teamSelect');
+
+// form submission 
+form.addEventListener('submit', function(event) {
